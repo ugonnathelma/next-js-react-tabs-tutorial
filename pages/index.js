bac5df4233f88b308cb7186a9233d2eec16b33d4
Ugonna Thelma <andela@Andelas-MacBook-Pro.local>
2019-03-10 00:53:17 +0100

    add pages

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../layouts"
+import Tabs from "../components/Tabs"
+
+const PageOne = props => {
+  return (
+    <Layout>
+      <Tabs />
+    </Layout>
+  )
+}
+
+export default PageOne
---
+++ pages/page-two.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../layouts"
+
+const PageTwo = () => {
+  return <Layout>Page 2 Content</Layout>
+}
+
+export default PageTwo
